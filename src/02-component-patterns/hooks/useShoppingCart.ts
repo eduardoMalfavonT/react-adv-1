7b4b11aa3043f024380c80f061281379f7cf136f
Eduardo Malfavon Talavera <malfavontalaveraj@hotmail.com>
2022-03-26 17:00:23 -0600

    Control props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -14,29 +14,12 @@ export const useShoppingCart = () => {
   }) => {
     // console.log( count, product);
     setShoppingCart((oldShippingCart) => {
-      const productInCart: ProductInCart = oldShippingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShippingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShippingCart;
+        return rest;
+      } else {
+        return { ...oldShippingCart, [product.id]: { ...product, count } };
       }
-
-      // Borrar el producto del carrito
-
-      const { [product.id]: toDelete, ...rest } = oldShippingCart;
-      return rest;
-
-      // if (count === 0) {
-      // const { [product.id]: toDelete, ...rest } = oldShippingCart;
-      // return rest;
-      // } else {
-      //   return { ...oldShippingCart, [product.id]: { ...product, count } };
-      // }
     });
   };
   return { shoppingCart, onProductCountChange };
